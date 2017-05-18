8258ba7e27c11f481e1863aa60192fa14cdb389b
zhangmiaojie <136218949@qq.com>
2017-05-18 17:55:25 +0800

    报表的实现，还未完成

---
+++ src/main/java/com/water/bocai/db/controller/ChartController.java
@@ -0,0 +1,14 @@
+package com.water.bocai.db.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhangmiaojie on 2017/5/18.
+ */
+@Controller
+@RequestMapping(value = "/chart")
+public class ChartController {
+
+//    public void
+}
